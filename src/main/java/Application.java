3f3358c51b161f65ccb2d94aa04072e4648326d5
GalchenkoYaroslav <yarosgalchenko@gmail.com>
2017-03-09 02:51:08 +0200

    first commit

---
+++ src/main/java/Application.java
@@ -0,0 +1,19 @@
+/**
+ * Created by Yaroslav on 09.03.2017.
+ */
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+@ComponentScan("com.java.tuiselect")
+@EnableAutoConfiguration
+public class Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Application.class, args);
+    }
+
+}
---
+++ src/main/java/com/java/tuiselect/rest/Greeting.java
@@ -0,0 +1,24 @@
+package com.java.tuiselect.rest;
+
+/**
+ * Created by Yaroslav on 09.03.2017.
+ */
+
+public class Greeting {
+
+    private final long id;
+    private final String content;
+
+    public Greeting(long id, String content) {
+        this.id = id;
+        this.content = content;
+    }
+
+    public long getId() {
+        return id;
+    }
+
+    public String getContent() {
+        return content;
+    }
+}
